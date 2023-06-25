b882d9e8e53fb595d43e3ab2e45c59e211f1c10d
Ubuntu <ubuntu@ip-172-31-88-251.ec2.internal>
2023-06-25 06:41:04 +0000

    newcommit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload data to get the latest webpage to help ukraine people or community with latest chnages.
         </p>
         <a
           className="App-link"
